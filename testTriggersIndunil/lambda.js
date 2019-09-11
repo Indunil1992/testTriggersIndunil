1054bc6310f7d9abc2af664f5988f87379169060
Indunil1992 <indunil@adroitlogic.com>
2019-09-11 13:27:43 +0530

    cloudWatch&ses

--- testTriggersIndunil/lambda.js
+++ testTriggersIndunil/lambda.js
@@ -1,21 +1,29 @@
 let AWS = require('aws-sdk');
-const sns = new AWS.SNS();
+const ses = new AWS.SES();
 
 exports.handler = function (event, context, callback) {
-    if (event) {
-        callback(event);
-    }
-    sns.subscribe({
-        Protocol: 'https',
-        Endpoint: 'https://mf4e09unvf.execute-api.eu-west-1.amazonaws.com/Test/qa/indu	',
-        TopicArn: 'arn:aws:sns:eu-west-1:318300609668:i'
-    }).promise()
-        .then(data => {
-            // your code goes here
-        })
-        .catch(err => {
-            // error handling goes here
-        });
+    ses.sendEmail({
+        Destination: {
+            ToAddresses: ['indunil@adroitlogic.com'],
+            CcAddresses: [],
+            BccAddresses: []
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: `sample ses mail from Ireland`
+                }
+            },
+            Subject: {
+                Data: 'test'
+            }
+        },
+        Source: 'indunil@adroitlogic.com',
+    }, function (err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else console.log(data);           // successful response
+    });
 
-    callback(null, { "message": "Successfully executed" });
+    callback(null, { "message": "Successfully executed cloudWatch" });
+
 }
